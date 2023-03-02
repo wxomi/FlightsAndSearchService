b9709c931ec8bfdbe98bd95d105a8e4209905bf2
Anshul Saini <anshulsaini@Anshuls-MacBook-Air.local>
2023-03-02 17:53:32 +0530

    Complete Basic sequelize Setup for creating a DB

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-const PORT = require("./config/serverConfig");
+const { PORT } = require("./config/serverConfig");
 
 const createAndStartServer = async () => {
   //create the express direct
   const app = express();
-  const PORT = process.env.PORT;
   app.listen(PORT, () => {
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
     console.log(`Server Started Successfully at ${PORT} .`);
   });
 };
