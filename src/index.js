df914a4de0495ae30b1700ae1a143ef4d097d0c0
Anshul Saini <anshulsaini@192.168.1.8>
2023-03-04 14:41:19 +0530

    Added Unique Constraint and Started City Repository

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 
 const { PORT } = require("./config/serverConfig");
+const CityRepository = require("./repository/city-repository");
 
-const createAndStartServer = async () => {
+const setupAndStartServer = async () => {
   //create the express direct
   const app = express();
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     console.log(`Server Started Successfully at ${PORT} .`);
   });
 };
 
-createAndStartServer();
+setupAndStartServer();
